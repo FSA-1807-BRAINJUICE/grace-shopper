4ed66a461d3dacaada46368065746285e2a7bb08
Han Ko <hanseok87@gmail.com>
2018-09-12 10:49:54 -0400

    finished basic users-routes

--- server/api/users.js
+++ server/api/users.js
@@ -4,12 +4,7 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
+    const users = await User.findAll()
     res.json(users)
   } catch (err) {
     next(err)
@@ -18,20 +13,49 @@ router.get('/', async (req, res, next) => {
 
 // get a user by the given id
 router.get('/:userId', async(req, res, next) => {
-
+  try {
+    const oneUser = await User.findById(req.params.userId)
+    res.json(oneUser)
+  } catch (err) {
+    next(err)
+  }
 });
 
 // create a new user
 router.post('/', async (req, res, next) => {
-
+  try {
+    if(!req.user.admin){ // req.user coming from PASSPORT SESSION
+      res.sendStatus(403)
+    }
+    const user = await User.create(req.body)
+    res.send(user)
+  } catch (error) {
+    next(error)
+  }
 });
 
 // update
 router.put('/:userId', async (req, res, next) => {
+  try {
+    if(!req.user.admin){
+      res.sendStatus(403)
+    }
 
+    const user = await User.update(req.body, {
+      where: {
+        id: req.params.userId
+      },
+      returning: true,
+      plain: true
+    });
+    if (!user) return res.sendStatus(404)
+    res.send(user);
+  } catch (err) {
+    next(err)
+  }
 });
 
 //get orders of this user(/:userId/orders?status=[pending/complete]);
-router.put('/:userId/orders', async (req, res, next) => {
+// router.put('/:userId/orders', async (req, res, next) => {
 
-})
+// })
