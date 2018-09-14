73a6b3908a58a867ec7373bfab2a36596396e4c7
Ryan Johnson <ryanpjohnson14@gmail.com>
2018-09-14 16:40:53 -0400

    440 friday cart cartitem components with updated master

--- client/app.js
+++ client/app.js
@@ -4,23 +4,14 @@ import {Navbar, Login, Signup} from './components'
 import AllProducts from './components/AllProducts'
 import {Route} from 'react-router-dom'
 import Routes from './routes'
-<<<<<<< HEAD
-import Cart from './components/Cart'
-=======
 import { Route, Switch } from 'react-router-dom'
 import SingleProduct from './components/SingleProduct'
 
->>>>>>> 3acab43f599cac5c47f51de6591ef397fd8148f2
 
 const App = () => {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
-      <Route path='/cart' component={Cart}/>
-      {/* <AllProducts /> */}
-      <Routes />
-=======
       {/* <Routes /> */}
 
       <Switch>
@@ -29,7 +20,6 @@ const App = () => {
         <Route path = '/products/:productId' component = {SingleProduct}/>
         <Route exact path = '/products' component = {AllProducts}/>
       </Switch>
->>>>>>> 3acab43f599cac5c47f51de6591ef397fd8148f2
 
     </div>
   )
