adcdbf25de9c1464e7fb1b2e0ba3494f1539b518
mina-gh <minamail@gmail.com>
2018-09-13 23:01:27 -0400

    skeleton and started working on addITem function

--- client/services/OrderService.js
+++ client/services/OrderService.js
@@ -1,12 +1,33 @@
 import axios from 'axios'
 
+
 class CartService {
   constructor(){
   }
 
-  addItem(item){
+  async addItem(item){
     // check if a user is logged-in
+    // if a user is logged-out, then check if there is a cookie for an order.
 
+    const cartId = 'cartId';
+
+
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie =  "order=" + cartId + ";" + expires + ";path=/";
+
+
+    // create a new order if needed
+    const res = await axios.post('/api/order');
+    const order = res.data;
+    //cartId = data.id;
+
+    // add the item to the cart;
+    const itemRes = await axios.post(`/api/order/${cartId}/item`);
+    const items = itemRes.data;
+
+    return items;
   }
 
   deleteItem(itemId){
