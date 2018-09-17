0404de25302b79c8ec3869bd5530cc73a3a19c5f
Ryan Johnson <ryanpjohnson14@gmail.com>
2018-09-17 15:38:34 -0400

    frontside remove cartitem button complete

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux'
-import {addItemToCart, updateItem} from '../store/cart'
+import {addItemToCart, updateItem, deleteItem} from '../store/cart'
 
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import { throws } from "assert";
 
 class CartItem extends Component {
   constructor(props) {
@@ -11,17 +12,25 @@ class CartItem extends Component {
     this.state = {
       quantity: this.props.cartItem.quantity
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
   }
   handleSubmit(evt) {
     evt.preventDefault();
     // const newCartItem = [this.props.name, this.props.price, this.props.imgUrl,this.props.description, this.state.quantity];
     // this.props.addItemToCart(newCartItem)
-    this.props.updateQuantity(this.props.cartItem, this.state.quantity)
+    this.props.updateQuantity(this.props.cartItem, this.state.quantity);
   }
+
+  handleRemove (evt) {
+    evt.preventDefault();
+    this.props.deleteItem(this.props.cartItem);
+  }
+
 
   render() {
     const { name, price, imgUrl, description } = this.props.cartItem.product;
+    //add accompany table cell in cart header?
     return (
         <TableRow className='cart-item'>
         <TableCell className='cart-item-img'><img src={imgUrl}/></TableCell>
@@ -34,6 +43,10 @@ class CartItem extends Component {
             <button className='orderItemSelectButton' type='submit'>Change Qty.</button>
           </form>
         </TableCell>
+        <TableCell className='cart-item-remove'>
+            <button className='remvoe-order-item-button' type='button' onClick={this.handleRemove}>Remove Item</button>
+        </TableCell>
+
       </TableRow>
     )
   }
@@ -42,7 +55,8 @@ class CartItem extends Component {
 const mapDispatchToProps = dispatch => {
   return {
     addItemToCart: (item) => dispatch(addItemToCart(item)),
-    updateQuantity: (item, quantity) => dispatch(updateItem(item,quantity))
+    updateQuantity: (item, quantity) => dispatch(updateItem(item,quantity)),
+    deleteItem: (item) => dispatch(deleteItem(item))
   }
 }
 
