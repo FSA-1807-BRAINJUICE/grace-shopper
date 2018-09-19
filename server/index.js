d0601206151da019c3edf00d79a8ea7b8ac75d32
mina-gh <minamail@gmail.com>
2018-09-19 01:27:25 -0400

    more try-catch

--- server/index.js
+++ server/index.js
@@ -108,21 +108,25 @@ const startListening = () => {
 const syncDb = () => db.sync()
 
 async function bootApp() {
-  try{
-    await sessionStore.sync()
-    await syncDb()
-    await createApp()
-    await startListening()
-  }catch(err){
-    console.log(err);
-  }
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
 }
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
 if (require.main === module) {
-  bootApp()
+  try{
+    bootApp()
+  }catch(err){
+    console.log(err);
+  }
 } else {
-  createApp()
+  try{
+    createApp()
+  }catch(err){
+    console.log(err);
+  }
 }
